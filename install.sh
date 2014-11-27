008321d137d9a56ac50d5216652e2ce9aaacb3fd
David Dworken <david@daviddworken.com>
2014-11-26 21:53:40 -0500

    Updated dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget -O apmplanner.deb http://ardupilot.com/wp-content/plugins/download-monitor/download.php?id=104
 apt-get update
-apt-get -y install git qt5-qmake qt5-default qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev libqt5svg5-dev libsdl1.2-dev  libsndfile-dev flite1-dev libssl-dev libudev-dev libsdl2-dev libopenthreads14 libphonon4 libqt4-declarative libqt4-network libqt4-opengl libqt4-script libqt4-sql libqt4-svg libqt4-test libqt4-xml libqtcore4 libqtgui4 libqtwebkit4
+apt-get -y install libopenscenegraph-dev git qt5-qmake qt5-default qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev libqt5svg5-dev libsdl1.2-dev  libsndfile-dev flite1-dev libssl-dev libudev-dev libsdl2-dev libopenthreads14 libphonon4 libqt4-declarative libqt4-network libqt4-opengl libqt4-script libqt4-sql libqt4-svg libqt4-test libqt4-xml libqtcore4 libqtgui4 libqtwebkit4
 dpkg -i apmplanner.deb
 cd ..
 rm -r installer
