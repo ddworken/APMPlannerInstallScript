c10839baa4ec265816b0935cfe1f02735c468405
David Dworken <david@daviddworken.com>
2014-11-26 21:20:17 -0500

    Removed curl dependency and changed directories

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-curl -o ~/Downloads/apmplanner.deb http://ardupilot.com/wp-content/plugins/download-monitor/download.php?id=104
+wget -O=apmplanner.deb http://ardupilot.com/wp-content/plugins/download-monitor/download.php?id=104
 apt-get update
 apt-get -y install git qt5-qmake qt5-default qtscript5-dev libqt5webkit5-dev libqt5serialport5-dev libqt5svg5-dev libsdl1.2-dev  libsndfile-dev flite1-dev libssl-dev libudev-dev libsdl2-dev
-dpkg -i ~/Downloads/apmplanner.deb -y
-rm ~/Downloads/apmplanner.deb
+dpkg -i apmplanner.deb -y
+cd ..
+rm -r installer
